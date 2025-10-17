ec1b6a8686c9b0b08c8c3ecbee1b1d79491b8ed9
MrMancs <marcell.szatmari2007@gmail.com>
2025-10-17 11:20:48 +0200

    Create basic backend methods

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -2,7 +2,20 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 
+app.use(cors())
+app.use(express.json())
+
 const port = 3333
+
+app.post("/api/result", (req, res) => {
+    console.log(req.body)
+    res.sendStatus(200)
+    
+})
+
+app.get("/api/results", (req, res) => {
+    res.sendStatus(200)
+})
 
 app.listen(port, ()=> {
     console.log("Szerver mükszik itt: ", port)
